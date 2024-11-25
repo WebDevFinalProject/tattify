d88a8435b5c8e9c1c8f940ec7a56357657e5cdaa
urooj <urooj.shareef@hotmail.com>
2024-11-25 11:25:22 +0100

    login registration added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,1 +1,13 @@
 // routes
+
+import express from "express"
+import { registration, userLogin } from "../controllers/userController.js" 
+
+const router = express.Router()
+
+
+router.post("/signup",registration)
+
+router.post("/login",userLogin)
+
+export default router
