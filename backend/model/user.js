b4039c3152823848de5495c90b6c802544e7db2b
urooj <urooj.shareef@hotmail.com>
2024-11-25 10:41:53 +0100

    userSchema added

--- backend/model/user.js
+++ backend/model/user.js
@@ -1,1 +1,32 @@
-// USER-SCHEMA
+// USER-SCHEma
+
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["customer", "artist"],
+    required: true,
+  },
+  profileImage: String,
+});
+
+const User = mongoose.model("Users", userSchema);
+export default User;
