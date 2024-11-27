add4e23dc8c15bb304ba45b8b065287fd47cf7d6
Dominik <m.dominik.andras@gmail.com>
2024-11-27 11:49:29 +0100

    added Contact comp to Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Contact from "../components/Home/Contact";
 
 const Home = () => {
-    return <></>;
+    return (
+        <>
+            <Contact />
+        </>
+    );
 };
 
 export default Home;
