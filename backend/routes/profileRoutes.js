532029e373f56b0d0a04a63e14de22859d807afe
urooj <urooj.shareef@hotmail.com>
2024-11-27 14:55:55 +0100

    artistProfile update and delete

--- backend/routes/profileRoutes.js
+++ backend/routes/profileRoutes.js
@@ -1,11 +1,21 @@
-import express from "express"
-import { createArtistProfile, getArtistProfile } from "../controllers/profileController.js";
+import express from "express";
+import {
+  createArtistProfile,
+  deleteArtistProfile,
+  getArtistProfile,
+  updateArtistProfile,
+} from "../controllers/profileController.js";
 import { auth } from "../middleware/auth.js";
+import { publicProfile } from "../controllers/publicController.js";
 
 const router = express.Router();
 
-router.post("/artists/profile",auth,createArtistProfile)
- router.get("/artists", auth, getArtistProfile)
+router.post("/artists/create-profile", auth, createArtistProfile);
+router.get("/artists/profile", auth, getArtistProfile);
+router.delete("/artists/:artistId", auth, deleteArtistProfile);
+router.put("/artists/:artistId", auth, updateArtistProfile);
 
 
- export default router
+router.get("/artist/profile/:id", publicProfile);
+
+export default router;
