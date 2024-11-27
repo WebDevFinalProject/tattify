7f0279ab748e6945397e28ce30f45e9cbba9b686
AizStein <absteinhoff@gmx.de>
2024-11-27 12:05:52 +0100

    public-profile

---
+++ backend/controllers/publicController.js
@@ -0,0 +1,64 @@
+import ArtistPortfolio from "../model/profile.js";
+import User from "../model/user.js";
+
+export const publicProfile = async (req, res) => {
+  try {
+    const artistId = req.params.id;
+
+    const artistUser = await User.findById(artistId).select(
+      "role firstName lastName profileImage portfolio"
+    );
+
+    if (!artistUser || artistUser.role !== "artist") {
+      return res
+        .status(404)
+        .json({ message: "Artist not found or not an artist!" });
+    }
+
+    const artist = await ArtistPortfolio.findOne({ user: artistId })
+      .populate("user", "firstName lastName profileImage")
+      .populate({
+        path: "reviews", // Populate the reviews and specific fields within reviews
+        populate: {
+          path: "customer", // Populate the customer reference in reviews
+          select: "firstName",
+        },
+      })
+      .select(
+        "bio specialties experience certifications languagesSpoken city country studioLocation basePrice pricingDetails socialLinks isAvailable"
+      );
+
+    const reviews = artist.reviews.map((review) => ({
+      rating: review.rating,
+      comment: review.comment,
+      createdAt: review.createdAt,
+      customer: {
+        firstName: review.customer.firstName,
+      },
+    }));
+
+    const response = {
+      firstName: artist.user.firstName,
+      lastName: artist.user.lastName,
+      profileImage: artist.user.profileImage,
+      bio: artist.bio,
+      specialties: artist.specialties,
+      experience: artist.experience,
+      certifications: artist.certifications,
+      languagesSpoken: artist.languagesSpoken,
+      city: artist.city,
+      country: artist.country,
+      studioLocation: artist.studioLocation,
+      basePrice: artist.basePrice,
+      pricingDetails: artist.pricingDetails,
+      socialLinks: artist.socialLinks,
+      isAvailable: artist.isAvailable,
+      portfolio: artistUser.portfolio,
+      reviews: reviews,
+    };
+
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
