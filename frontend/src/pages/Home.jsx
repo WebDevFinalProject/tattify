ab815a0eb2ef5e72f4227a88e8c90768431abfeb
Dominik <m.dominik.andras@gmail.com>
2024-12-02 14:26:49 +0100

    added Hero section, adjusted navbar

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,16 +4,17 @@ import Journey from "../components/Home/Journey";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import Suggestion from "../components/Home/Suggestion";
+import Hero from "../components/Home/Hero";
 const Home = () => {
-  return (
-    <>
-      <NavBar />
-      <Journey />
-      <Suggestion />
-      <Contact />
-
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <NavBar />
+            <Hero />
+            <Journey />
+            <Suggestion />
+            <Contact />
+            <Footer />
+        </>
+    );
 };
 export default Home;
