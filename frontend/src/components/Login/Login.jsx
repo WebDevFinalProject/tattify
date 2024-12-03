8bdca5ce0d057d3813d7a048af9f872bc736a6ed
urooj <urooj.shareef@hotmail.com>
2024-12-03 13:58:38 +0100

    registration completed

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -4,41 +4,35 @@ import { Link } from "react-router-dom";
 
 function Login() {
   return (
-    <div className="login-page">
-      <div className="container-login">
-        <div className="logo">
-          <h1>Tattify</h1>
-          <h2>For users:</h2>
-          <ul>
-            <li>Connect with artists.</li>
-            <li> Search for a location.</li>
-            <li> Leave a review.</li>
-          </ul>
-          <h2>Are you an artist?</h2>
-          <ul>
-            <li> Show your work.</li>
-            <li>Connect with users.</li>
-            <li>Stay recognizable.</li>
-          </ul>
-        </div>
-        <div className="login-form">
-          <form className="form">
-            <input type="email" placeholder="Enter your Email" required />
-            <input type="password" placeholder="Enter your password" required />
+    <div className="container-login">
+      <div className="logo">
+        <h1>Tattify</h1>
+        <h2>For users:</h2>
+        <p>Connect with artists</p>
+        <p>Search for a location</p>
+        <p>Leave a review</p>
+        <h2>Are you an artist?</h2>
+        <p> Show your work.</p>
+        <p>Connect with users</p>
+        <p>Stay recognizable</p>
+      </div>
+      <div className="login-form">
+        <form className="form">
+          <input type="email" placeholder="Enter your Email" required />
+          <input type="password" placeholder="Enter your password" required />
 
-            <a href="#" className="string">
-              Forgot password?
-            </a>
-          </form>
-
-          <button type="submit" id="login">
-            Log In
-          </button>
-          <div className="divider"></div>
-          <a href="#" className="string">
-            Create an account
+          <a href="#" className="forgot-password">
+            Forgot password?
           </a>
-        </div>
+        </form>
+
+        <button type="submit" id="login">
+          Log In
+        </button>
+        <div className="divider"></div>
+        <button type="button" className="create-account">
+          Registration
+        </button>
       </div>
     </div>
   );
