3b854f7c14be7c009f056dcaffe6d5d057d91a66
Dominik <m.dominik.andras@gmail.com>
2024-12-04 14:02:39 +0100

    worked on hero text

--- frontend/src/components/Home/Hero.jsx
+++ frontend/src/components/Home/Hero.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import "./styles/hero.css";
 import HeroImage from "../../assets/hero-image.jpg";
+import HeroText from "../../assets/hero-text.png";
 
 const HeroSection = () => {
     return (
         <div className="hero-container">
             <div className="text-container">
-                <h1>Tattify</h1>
+                {/* <h1>Tattify</h1>
                 <p>Find a tattoo artist today</p>
-                <button>Start looking!</button>
+                <button>Start looking!</button> */}
+                <img src={HeroText} alt="Logo" />
             </div>
             <div className="image-container">
                 <img src={HeroImage} alt="Tattooed Model" />
