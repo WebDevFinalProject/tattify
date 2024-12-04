64abac7c6f8e9b972665e9ae03d36328ea4ad93d
AizStein <absteinhoff@gmx.de>
2024-12-04 13:55:25 +0100

    nav-breakpoints-done

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,23 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NavBar.css";
 import { NavLink } from "react-router-dom";
+import { FiMenu, FiX } from "react-icons/fi";
 
 function NavBar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const navOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
       <div className="navbar-container">
         <div className="logo-container">
           <h1>Tattify</h1>
         </div>
-        <nav>
+        <div className="nav-icon" onClick={navOpen}>
+          {isOpen ? <FiX size={28} /> : <FiMenu size={28} />}
+        </div>
+        <nav className={`nav-links ${isOpen ? "open-nav" : ""}`}>
+          <div className="nav-header-dropdown">
+            <h2>Tattify</h2>
+          </div>
           <NavLink>Home</NavLink>
           <NavLink>About</NavLink>
           <NavLink>Contact</NavLink>
           <NavLink to="/articles">Article</NavLink>
         </nav>
         <div className="button-container">
-        <button><NavLink to="/login">Login/Register</NavLink></button>
-         </div>
+          <button>
+            <NavLink to="/login">Login/Register</NavLink>
+          </button>
+        </div>
       </div>
     </>
   );
