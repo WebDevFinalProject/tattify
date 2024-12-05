c3f967d6626a88c1c5e4ac0310b9e3f97586dafe
AizStein <absteinhoff@gmx.de>
2024-12-05 11:25:01 +0100

    userContext added

--- frontend/src/context/contextProvider.jsx
+++ frontend/src/context/contextProvider.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { createContext } from "react";
-import api from "../api";
+import api from "../components/api";
 
 export const UserContext = createContext();
 
