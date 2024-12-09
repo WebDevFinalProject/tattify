50785a6b346cd464bb685f40d9e7012883fb29b0
Viktoriya <viktoriabron@gmail.com>
2024-12-09 13:39:49 +0100

    added some styles to ArtistForm

--- frontend/src/components/ArtistProfile/ArtistForm.jsx
+++ frontend/src/components/ArtistProfile/ArtistForm.jsx
@@ -48,90 +48,103 @@ const ArtistForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="fromArtist">
-      <input
-        type="text"
-        name="bio"
-        value={formData.bio}
-        onChange={handleChange}
-        placeholder="Bio"
-      />
-      <input
-        className="form-input"
-        type="text"
-        name="specialties"
-        value={formData.specialties}
-        onChange={handleChange}
-        placeholder="Specialties"
-      />
-      <input
-        className="form-input"
-        type="text"
-        name="city"
-        value={formData.city}
-        onChange={handleChange}
-        placeholder="City"
-      />
-      <input
-        className="form-input"
-        type="text"
-        name="country"
-        value={formData.country}
-        onChange={handleChange}
-        placeholder="Country"
-      />
-      <input
-        className="form-input"
-        type="text"
-        name="basePrice"
-        value={formData.basePrice}
-        onChange={handleChange}
-        placeholder="Base Price"
-      />
-      <input
-        className="form-input"
-        type="text"
-        name="certifications"
-        value={formData.certifications}
-        onChange={handleChange}
-        placeholder="Certifications"
-      />
-      <input
-        className="form-input"
-        type="text"
-        name="languagesSpoken"
-        value={formData.languagesSpoken}
-        onChange={handleChange}
-        placeholder="Languages Spoken"
-      />
-      <input
-        className="form-input"
-        type="text"
-        name="studioLocation"
-        value={formData.studioLocation}
-        onChange={handleChange}
-        placeholder="Studio Location"
-      />
-      <input
-        className="form-input"
-        type="text"
-        name="pricingDetails"
-        value={formData.pricingDetails}
-        onChange={handleChange}
-        placeholder="Pricing Details"
-      />
-      <input
-        className="form-input"
-        type="text"
-        name="socialLinks"
-        value={formData.socialLinks}
-        onChange={handleChange}
-        placeholder="Social Links"
-      />
-      <button type="submit" className="form-button">
-        Create Profile
-      </button>
-    </form>
+    <section className="form-artist-create">
+      <form onSubmit={handleSubmit} className="fromArtist">
+        <label htmlFor="specialties">Styles:</label>
+        <input
+          className="form-input"
+          type="text"
+          name="specialties"
+          value={formData.specialties}
+          onChange={handleChange}
+          placeholder="Styles"
+        />
+        <label htmlFor="city">City:</label>
+        <input
+          className="form-input"
+          type="text"
+          name="city"
+          value={formData.city}
+          onChange={handleChange}
+          placeholder="City"
+        />
+        <label htmlFor="country">Country:</label>
+        <input
+          className="form-input"
+          type="text"
+          name="country"
+          value={formData.country}
+          onChange={handleChange}
+          placeholder="Country"
+        />
+        <label htmlFor="basePrice">Base Price:</label>
+        <input
+          className="form-input"
+          type="text"
+          name="basePrice"
+          value={formData.basePrice}
+          onChange={handleChange}
+          placeholder="Base Price"
+        />
+        <label htmlFor="certifications">Certifications:</label>
+        <input
+          className="form-input"
+          type="text"
+          name="certifications"
+          value={formData.certifications}
+          onChange={handleChange}
+          placeholder="Certifications"
+        />
+        <label htmlFor="languagesSpoken">Languages Spoken:</label>
+        <input
+          className="form-input"
+          type="text"
+          name="languagesSpoken"
+          value={formData.languagesSpoken}
+          onChange={handleChange}
+          placeholder="Languages Spoken"
+        />
+        <label htmlFor="studioLocation">Studio Location:</label>
+        <input
+          className="form-input"
+          type="text"
+          name="studioLocation"
+          value={formData.studioLocation}
+          onChange={handleChange}
+          placeholder="Studio Location"
+        />
+        <label htmlFor="pricingDetails">Pricing Details:</label>
+        <input
+          className="form-input"
+          type="text"
+          name="pricingDetails"
+          value={formData.pricingDetails}
+          onChange={handleChange}
+          placeholder="Pricing Details"
+        />
+        <label htmlFor="socialLinks">Social Links:</label>
+        <input
+          className="form-input"
+          type="text"
+          name="socialLinks"
+          value={formData.socialLinks}
+          onChange={handleChange}
+          placeholder="Social Links"
+        />
+        <label htmlFor="bio">Bio:</label>
+        <input
+          className="bio-input"
+          type="text"
+          name="bio"
+          value={formData.bio}
+          onChange={handleChange}
+          placeholder="Bio"
+        />
+        <button type="submit" className="form-button">
+          Save
+        </button>
+      </form>
+    </section>
   );
 };
 
