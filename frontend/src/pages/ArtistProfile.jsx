2ab3a54bdc6c8c7bdba020d1c9a73da694d89239
Dominik <m.dominik.andras@gmail.com>
2024-12-10 14:20:17 +0100

    added bio component and styling

--- frontend/src/pages/ArtistProfile.jsx
+++ frontend/src/pages/ArtistProfile.jsx
@@ -2,12 +2,26 @@ import React from "react";
 import NavBar from "../components/NavBar.jsx";
 import Footer from "../components/Footer.jsx";
 import MainInfo from "../components/ArtistProfile/MainInfo.jsx";
+import styled from "styled-components";
+import Bio from "../components/ArtistProfile/Bio.jsx";
+
+const Wrapper = styled.section`
+    padding: 0rem 4rem;
+    background: linear-gradient(
+        180deg,
+        rgb(229, 209, 188) 0%,
+        rgba(0, 0, 0, 0) 50%
+    );
+`;
 
 function PublicProfile() {
     return (
         <>
             <NavBar />
-            <MainInfo />
+            <Wrapper>
+                <MainInfo />
+                <Bio />
+            </Wrapper>
             <Footer />
         </>
     );
