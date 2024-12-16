c9d4f6b033d9335fabc64122fa3b070f30182e85
Viktoriya <viktoriabron@gmail.com>
2024-12-16 13:48:44 +0100

    ReviewHook added

---
+++ frontend/src/hooks/useReviewHook/useSubmitReview.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import  api from "../../components/api"
+
+const useSubmitReview=()=>{
+    const [loading,setloading] = useState(false);
+    const [error, setError] = useState(null);
+    const[success, setSuccess] = useState(false);
+
+
+    const submitReview = async (artistId, rating, comment)=>{
+        setloading(true);
+        setError(null);
+        setSuccess(false);
+
+       
+
+      try{
+        const response = await api.post(
+          "/api/submit-review",
+          {artistId, rating,comment},
+          { withCredentials: true })
+        setSuccess(true);
+        return response.data;
+      }catch(err){
+        setError(err.response?.data?.message || "Failed to submit review")
+      }finally{
+        setloading(false)
+      }
+    };
+    return {submitReview, loading,error, success}
+}
+
+
+export default useSubmitReview;
