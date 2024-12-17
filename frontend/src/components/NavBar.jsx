57c4bf754f534713acc7d136d114e08b2b334a8d
AizStein <absteinhoff@gmx.de>
2024-12-17 15:36:52 +0100

    nav-bar, nav-dropdown

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -4,24 +4,24 @@ import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import { FiMenu, FiX } from "react-icons/fi";
 import { useContext } from "react";
 import { UserContext } from "../context/ContextProvider";
-import { HiChat, HiLogout, HiUser } from "react-icons/hi";
+import { HiLogout } from "react-icons/hi";
 import useProfileImageUpload from "../hooks/useProfileImageUpload";
 import { FaCamera } from "react-icons/fa";
 import DropdownNav from "./Navigation/DropdownNav";
+import CustomSlideImages from "./Navigation/CustomSlideImages";
 
 function NavBar() {
   const location = useLocation();
-  const { user, clickHandlerVisibility, isOpen, logout } =
-    useContext(UserContext);
-  const [isProfileOpen, setProfileIsOpen] = useState(false);
+  const { user, logout } = useContext(UserContext);
+  const [activeDropdown, setActiveDropdown] = useState(null);
   const [isUploadDialogOpen, setUploadDialogOpen] = useState(false);
   const navigate = useNavigate();
 
   const { file, loading, error, response, handleSelectFile, handleUpload } =
     useProfileImageUpload();
 
-  const toggleProfile = () => {
-    setProfileIsOpen(!isProfileOpen);
+  const toggleDropdown = (dropdownName) => {
+    setActiveDropdown(activeDropdown === dropdownName ? null : dropdownName);
   };
 
   const toggleUploadDialog = () => {
@@ -56,47 +56,58 @@ function NavBar() {
         <div className="logo-container">
           <h1>Tattify</h1>
         </div>
-        <div className="nav-icon" onClick={clickHandlerVisibility}>
-          {isOpen ? <FiX size={28} /> : <FiMenu size={28} />}
+        <div className="nav-icon" onClick={() => toggleDropdown("nav")}>
+          {activeDropdown === "nav" ? <FiX size={28} /> : <FiMenu size={28} />}
         </div>
-        <nav className={`nav-links ${isOpen ? "open-nav" : ""}`}>
+        <nav
+          className={`nav-links ${activeDropdown === "nav" ? "open-nav" : ""}`}
+        >
           <div className="nav-header-dropdown">
             <h2>Tattify</h2>
           </div>
           <NavLink to="/">Home</NavLink>
-          <NavLink>About</NavLink>
+          <NavLink to="#">About</NavLink>
           <NavLink to="/#contact-form">Contact</NavLink>
           <NavLink to="/articles">Article</NavLink>
         </nav>
         {user ? (
           <div className="nav-dropdown-container">
-            <div className="nav-account" onClick={toggleProfile}>
+            <div
+              className="nav-account"
+              onClick={() => toggleDropdown("profile")}
+            >
               <img
                 src={user.profileImage}
                 alt="User Profile"
                 className="user-profile-img"
               />
-              <FaCamera onClick={toggleUploadDialog} className="camera-icon" />
+              <FaCamera
+                onClick={(e) => {
+                  e.stopPropagation();
+                  toggleUploadDialog();
+                }}
+                className="camera-icon"
+              />
             </div>
 
-            <div
-              className={`nav-profile-path ${
-                isProfileOpen ? "open-profile-dropdown" : ""
-              }`}
-            >
-              <div className="account-dropdown-menu">
-                {user && (
-                  <>
-                    <h2 className="customer-name">{`${user.firstName} ${user.lastName}`}</h2>
-                    <DropdownNav />
-                  </>
-                )}
-
-                <button className="nav-button-logout" onClick={logoutHandler}>
-                  <HiLogout size={21} /> &nbsp; Logout
-                </button>
+            {activeDropdown === "profile" && (
+              <div className="nav-profile-path open-profile-dropdown">
+                <div className="account-dropdown-menu">
+                  <CustomSlideImages />
+                  {user && (
+                    <>
+                      <DropdownNav />
+                      <button
+                        className="nav-button-logout"
+                        onClick={logoutHandler}
+                      >
+                        <HiLogout size={21} /> &nbsp; Logout
+                      </button>
+                    </>
+                  )}
+                </div>
               </div>
-            </div>
+            )}
           </div>
         ) : (
           <div className="button-container">
--- frontend/src/components/Navigation/DropdownNav.jsx
+++ frontend/src/components/Navigation/DropdownNav.jsx
@@ -1,20 +1,46 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { UserContext } from "../../context/ContextProvider";
 import { NavLink } from "react-router-dom";
+import { HiChat, HiUser } from "react-icons/hi";
+import { FaPeopleArrows } from "react-icons/fa";
 
 const DropdownNav = () => {
   const { user, isLoading } = useContext(UserContext);
+  const userId = user?.id;
+
+  console.log(userId);
 
   if (isLoading) {
     return <div>loading...</div>;
   }
   return (
     <>
-      {user && user.role === "customer" && (
-        <NavLink to="/customer-profile">Artist</NavLink>
-      )}
-      {user && user.role === "artist" && (
-        <NavLink to={`/artist-profile/${user._id}`}>Profile</NavLink>
+      {user && (
+        <>
+          <h2 className="customer-name">{`Welcome, ${user.firstName} ${user.lastName}!`}</h2>
+          <NavLink
+            className="nav-profile-links"
+            to={
+              user?.role === "artist" ? `/artist-profile/${userId}` : "/artists"
+            }
+            key={userId}
+          >
+            {user.role === "artist" ? (
+              <div>
+                <HiUser />
+                &nbsp; Your Profile
+              </div>
+            ) : (
+              <div>
+                <FaPeopleArrows />
+                &nbsp; Find your match!
+              </div>
+            )}
+          </NavLink>
+          <NavLink className="chat-link">
+            <HiChat /> &nbsp; Messages
+          </NavLink>
+        </>
       )}
     </>
   );
