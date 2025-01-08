73c300703930e669bd994e608ba69a487894cd9e
Dominik <m.dominik.andras@gmail.com>
2025-01-08 14:13:14 +0100

    optimized styling of mainInfo and Portfolio

--- frontend/src/components/ArtistProfile/Portfolio.jsx
+++ frontend/src/components/ArtistProfile/Portfolio.jsx
@@ -5,58 +5,65 @@ import { BiSolidCameraPlus } from "react-icons/bi";
 import { UserContext } from "../../context/ContextProvider";
 import { useParams } from "react-router-dom";
 import useImageDelete from "../../hooks/useImageDelete";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
 function Portfolio({ artist }) {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const { deleteImageHandler } = useImageDelete();
-  const { user, isOpen, clickHandlerVisibility } = useContext(UserContext);
-  const { id } = useParams();
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const { deleteImageHandler } = useImageDelete();
+    const { user, isOpen, clickHandlerVisibility } = useContext(UserContext);
+    const { id } = useParams();
 
-  const isOwner = user && user?._id === id;
+    const isOwner = user && user?._id === id;
 
-  // Toggle Modal
-  const toggleModal = () => setIsModalOpen(!isModalOpen);
+    // Toggle Modal
+    const toggleModal = () => setIsModalOpen(!isModalOpen);
 
-  return (
-    <>
-      {artist ? (
-        <div className="artist-portfolio-main-container">
-          {/* Portfolio Heading */}
-          <div className="portfolio-header">
-            <H2>Portfolio</H2>
-            {isOwner && (
-              <BiSolidCameraPlus
-                className="upload-icon"
-                onClick={toggleModal}
-              />
+    return (
+        <>
+            {artist ? (
+                <div className="artist-portfolio-main-container">
+                    {/* Portfolio Heading */}
+                    <div className="portfolio-header">
+                        <H2>Portfolio</H2>
+                        {isOwner && (
+                            <BiSolidCameraPlus
+                                className="upload-icon"
+                                onClick={toggleModal}
+                            />
+                        )}
+                    </div>
+
+                    {/* Display Portfolio Images */}
+                    <PortfolioContainer>
+                        {artist.portfolio.map((imageUrl, index) => (
+                            <div key={index} className="portfolio-image-box">
+                                <img
+                                    index={index}
+                                    src={imageUrl}
+                                    alt={`Portfolio image ${index + 1}`}
+                                />
+                                {isOwner && (
+                                    <button
+                                        className="delete-button"
+                                        onClick={() =>
+                                            deleteImageHandler(imageUrl)
+                                        }
+                                    >
+                                        <FontAwesomeIcon icon={faXmark} />
+                                    </button>
+                                )}
+                            </div>
+                        ))}
+                    </PortfolioContainer>
+                </div>
+            ) : (
+                <p>Nothing to show here</p>
             )}
-          </div>
-
-          {/* Display Portfolio Images */}
-          <PortfolioContainer>
-            {artist.portfolio.map((imageUrl, index) => (
-              <div key={index} className="porfolio-image-box">
-                <img
-                  index={index}
-                  src={imageUrl}
-                  alt={`Portfolio image ${index + 1}`}
-                />
-                {isOwner && (
-                  <button onClick={() => deleteImageHandler(imageUrl)}>
-                    delete
-                  </button>
-                )}
-              </div>
-            ))}
-          </PortfolioContainer>
-        </div>
-      ) : (
-        <p>Nothing to show here</p>
-      )}
-      {/* Upload Modal */}
-      {isModalOpen && <UploadPortfolio onClose={toggleModal} />}
-    </>
-  );
+            {/* Upload Modal */}
+            {isModalOpen && <UploadPortfolio onClose={toggleModal} />}
+        </>
+    );
 }
 
 export default Portfolio;
