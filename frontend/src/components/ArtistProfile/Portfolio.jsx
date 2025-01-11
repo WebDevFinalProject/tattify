9e9114a5350833b8740926ea2ee95bebfdd97ae0
AizStein <absteinhoff@gmx.de>
2025-01-11 22:43:37 +0100

    portfolio improvement

--- frontend/src/components/ArtistProfile/Portfolio.jsx
+++ frontend/src/components/ArtistProfile/Portfolio.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { H2, PortfolioContainer } from "./styles/StyledComponents";
 import UploadPortfolio from "./UploadPortfolio";
 import { BiSolidCameraPlus } from "react-icons/bi";
@@ -7,63 +7,118 @@ import { useParams } from "react-router-dom";
 import useImageDelete from "../../hooks/useImageDelete";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Mousewheel, Keyboard } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 function Portfolio({ artist }) {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const { deleteImageHandler } = useImageDelete();
-    const { user, isOpen, clickHandlerVisibility } = useContext(UserContext);
-    const { id } = useParams();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const { deleteImageHandler } = useImageDelete();
+  const { user, isOpen, clickHandlerVisibility } = useContext(UserContext);
+  const { id } = useParams();
+  const [openImg, setOpenImg] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-    const isOwner = user && user?._id === id;
+  const isOwner = user && user?._id === id;
 
-    // Toggle Modal
-    const toggleModal = () => setIsModalOpen(!isModalOpen);
+  // Toggle Modal
+  const toggleModal = () => setIsModalOpen(!isModalOpen);
 
-    return (
-        <>
-            {artist ? (
-                <div className="artist-portfolio-main-container">
-                    {/* Portfolio Heading */}
-                    <div className="portfolio-header">
-                        <H2>Portfolio</H2>
-                        {isOwner && (
-                            <BiSolidCameraPlus
-                                className="upload-icon"
-                                onClick={toggleModal}
-                            />
-                        )}
-                    </div>
+  const openImage = (index) => {
+    setCurrentIndex(index);
+    setOpenImg(true);
+  };
+  const closeImage = () => setOpenImg(false);
 
-                    {/* Display Portfolio Images */}
-                    <PortfolioContainer>
-                        {artist.portfolio.map((imageUrl, index) => (
-                            <div key={index} className="portfolio-image-box">
-                                <img
-                                    index={index}
-                                    src={imageUrl}
-                                    alt={`Portfolio image ${index + 1}`}
-                                />
-                                {isOwner && (
-                                    <button
-                                        className="delete-button"
-                                        onClick={() =>
-                                            deleteImageHandler(imageUrl)
-                                        }
-                                    >
-                                        <FontAwesomeIcon icon={faXmark} />
-                                    </button>
-                                )}
-                            </div>
-                        ))}
-                    </PortfolioContainer>
-                </div>
-            ) : (
-                <p>Nothing to show here</p>
+  /* stop the background from scrolling */
+  useEffect(() => {
+    if (openImg) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  }, [openImg]);
+
+  return (
+    <>
+      {artist ? (
+        <div className="artist-portfolio-main-container">
+          {/* Portfolio Heading */}
+          <div className="portfolio-header">
+            <H2>Portfolio</H2>
+            {isOwner && (
+              <BiSolidCameraPlus
+                className="upload-icon"
+                onClick={toggleModal}
+              />
             )}
-            {/* Upload Modal */}
-            {isModalOpen && <UploadPortfolio onClose={toggleModal} />}
-        </>
-    );
+          </div>
+
+          {/* Display Portfolio Images */}
+          <PortfolioContainer>
+            {artist.portfolio.map((imageUrl, index) => (
+              <div key={index} className="portfolio-image-box">
+                <img
+                  index={index}
+                  src={imageUrl}
+                  alt={`Portfolio image ${index + 1}`}
+                  style={{ cursor: "pointer" }}
+                  onClick={() => openImage(index)}
+                />
+                {isOwner && (
+                  <button
+                    className="delete-button"
+                    onClick={() => deleteImageHandler(imageUrl)}
+                  >
+                    <FontAwesomeIcon icon={faXmark} />
+                  </button>
+                )}
+              </div>
+            ))}
+            {/* image clickable  */}
+            {openImg && (
+              <div className="modal">
+                <button className="close-button" onClick={closeImage}>
+                  Close
+                </button>
+                <Swiper
+                  initialSlide={currentIndex}
+                  navigation
+                  pagination={{ clickable: true }}
+                  spaceBetween={10}
+                  slidesPerView={1}
+                  modules={[Navigation, Mousewheel, Keyboard]}
+                  mousewheel={true}
+                  keyboard={true}
+                  className="swiper-container"
+                >
+                  {artist.portfolio.map((imageUrl, index) => (
+                    <SwiperSlide key={index}>
+                      <img
+                        src={imageUrl}
+                        alt={`Portfolio ${index + 1}`}
+                        style={{
+                          width: "100%",
+                          height: "80%",
+                          objectFit: "contain",
+                        }}
+                      />
+                    </SwiperSlide>
+                  ))}
+                </Swiper>
+              </div>
+            )}
+          </PortfolioContainer>
+        </div>
+      ) : (
+        <p>Nothing to show here</p>
+      )}
+      {/* Upload Modal */}
+      {isModalOpen && <UploadPortfolio onClose={toggleModal} />}
+    </>
+  );
 }
 
 export default Portfolio;
--- frontend/src/components/ArtistProfile/styles/StyledComponents.js
+++ frontend/src/components/ArtistProfile/styles/StyledComponents.js
@@ -2,108 +2,144 @@ import styled from "styled-components";
 
 // media queries
 const size = {
-    mobileS: "320px",
-    mobileM: "375px",
-    mobileL: "425px",
-    tablet: "768px",
-    laptop: "1024px",
-    laptopL: "1440px",
-    desktop: "2560px",
+  mobileS: "320px",
+  mobileM: "375px",
+  mobileL: "425px",
+  tablet: "768px",
+  laptop: "1024px",
+  laptopL: "1440px",
+  desktop: "2560px",
 };
 
 const device = {
-    mobileS: `(min-width: ${size.mobileS})`,
-    mobileM: `(min-width: ${size.mobileM})`,
-    mobileL: `(min-width: ${size.mobileL})`,
-    tablet: `(min-width: ${size.tablet})`,
-    laptop: `(min-width: ${size.laptop})`,
-    laptopL: `(min-width: ${size.laptopL})`,
-    desktop: `(min-width: ${size.desktop})`,
-    desktopL: `(min-width: ${size.desktop})`,
+  mobileS: `(min-width: ${size.mobileS})`,
+  mobileM: `(min-width: ${size.mobileM})`,
+  mobileL: `(min-width: ${size.mobileL})`,
+  tablet: `(min-width: ${size.tablet})`,
+  laptop: `(min-width: ${size.laptop})`,
+  laptopL: `(min-width: ${size.laptopL})`,
+  desktop: `(min-width: ${size.desktop})`,
+  desktopL: `(min-width: ${size.desktop})`,
 };
 
 export const Wrapper = styled.section`
-    padding: 0rem 4rem;
+  padding: 0rem 4rem;
+  background: linear-gradient(
+    180deg,
+    rgb(229, 209, 188) 0%,
+    rgba(0, 0, 0, 0) 18%
+  );
+
+  @media ${device.tablet} {
+    padding: 0rem 8%;
     background: linear-gradient(
-        180deg,
-        rgb(229, 209, 188) 0%,
-        rgba(0, 0, 0, 0) 18%
+      180deg,
+      rgb(229, 209, 188) 0%,
+      rgba(0, 0, 0, 0) 30%
     );
+    display: grid;
+    gap: 4rem;
+    grid-template-columns: 25% 75%;
+    grid-template-rows: auto;
+    grid-template-areas: "main main";
 
-    @media ${device.tablet} {
-        padding: 0rem 8%;
-        background: linear-gradient(
-            180deg,
-            rgb(229, 209, 188) 0%,
-            rgba(0, 0, 0, 0) 30%
-        );
-        display: grid;
-        gap: 4rem;
-        grid-template-columns: 25% 75%;
-        grid-template-rows: auto;
-        grid-template-areas: "main main";
-
-        > div:nth-child(1) {
-            grid-area: main;
-            justify-self: start;
-            display: flex;
-            flex-direction: row;
-            padding-left: 3rem;
-            div {
-                display: flex;
-                align-items: start;
-                padding-top: 14rem;
-                padding-left: 1rem;
-            }
-        }
+    > div:nth-child(1) {
+      grid-area: main;
+      justify-self: start;
+      display: flex;
+      flex-direction: row;
+      padding-left: 3rem;
+      div {
+        display: flex;
+        align-items: start;
+        padding-top: 14rem;
+        padding-left: 1rem;
+      }
     }
+  }
 `;
 
 // used for the artist profile
 export const H2 = styled.h2`
-    font-family: "Lato", sans-serif;
-    padding-left: 0.5rem;
-    margin-bottom: 1.5rem;
-    text-transform: uppercase;
-    width: 100%;
-    border-bottom: 1px solid black;
-    padding-bottom: 1rem;
+  font-family: "Lato", sans-serif;
+  padding-left: 0.5rem;
+  margin-bottom: 1.5rem;
+  text-transform: uppercase;
+  width: 100%;
+  border-bottom: 1px solid black;
+  padding-bottom: 1rem;
 `;
 
 export const PortfolioContainer = styled.div`
-    margin-bottom: 4rem;
+  margin-bottom: 4rem;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1.2rem;
+  justify-content: center;
+
+  .portfolio-image-box {
+    position: relative;
+
+    &:hover .delete-button {
+      opacity: 1;
+    }
+
+    .delete-button {
+      position: absolute;
+      bottom: 0;
+      right: 0px;
+      width: 2.4rem;
+      height: 2.4rem;
+      border: 2px solid grey;
+      border-radius: 0.3rem;
+      background-color: white;
+      opacity: 0;
+      transition: 0.7s ease;
+
+      svg {
+        transform: scale(1.3);
+      }
+    }
+  }
+
+  img {
+    max-width: 25rem;
+    object-fit: cover;
+  }
+
+  .modal {
+    position: fixed;
+    top: 100px;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background: rgba(0, 0, 0, 0.8);
     display: flex;
-    flex-wrap: wrap;
-    gap: 1.2rem;
     justify-content: center;
+    align-items: center;
+    z-index: 1000;
+  }
 
-    .portfolio-image-box {
-        position: relative;
+  .close-button {
+    position: absolute;
+    top: 30px;
+    right: 50px;
+    border: none;
+    padding: 10px 15px;
+    font-size: 16px;
+    cursor: pointer;
+    z-index: 1100;
+  }
 
-        &:hover .delete-button {
-            opacity: 1;
-        }
+  .swiper-container {
+    width: 80%;
+    height: 80%;
+  }
 
-        .delete-button {
-            position: absolute;
-            bottom: 0;
-            right: 0px;
-            width: 2.4rem;
-            height: 2.4rem;
-            border: 2px solid grey;
-            border-radius: 0.3rem;
-            background-color: white;
-            opacity: 0;
-            transition: 0.7s ease;
-
-            svg {
-                transform: scale(1.3);
-            }
-        }
-    }
-
-    img {
-        max-width: 25rem;
-        object-fit: cover;
-    }
+  .swiper-slide img {
+    display: block;
+    max-width: 100%;
+    max-height: 100%;
+    margin: auto;
+  }
 `;
