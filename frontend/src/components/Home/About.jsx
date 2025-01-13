e69c1f7287baf7a599ce4433836dc4964aa0d30c
Dominik <m.dominik.andras@gmail.com>
2025-01-13 14:50:25 +0100

    added about page

---
+++ frontend/src/components/Home/About.jsx
@@ -0,0 +1,100 @@
+import { CenteredWrap, StyledH2, StyledParagraph, Title } from "./styles/about";
+import useIntersectionObserver from "../../hooks/useIntersectionObserver";
+// import "./styles/about.css";
+
+const About = () => {
+    const [titleRef, titleVisible] = useIntersectionObserver({
+        threshold: 0.1,
+    });
+    const [missionRef, missionVisible] = useIntersectionObserver({
+        threshold: 0.1,
+    });
+    const [paragraphRef1, paragraphVisible1] = useIntersectionObserver({
+        threshold: 0.1,
+    });
+    const [paragraphRef2, paragraphVisible2] = useIntersectionObserver({
+        threshold: 0.1,
+    });
+    const [paragraphRef3, paragraphVisible3] = useIntersectionObserver({
+        threshold: 0.1,
+    });
+    const [paragraphRef4, paragraphVisible4] = useIntersectionObserver({
+        threshold: 0.1,
+    });
+    const [paragraphRef5, paragraphVisible5] = useIntersectionObserver({
+        threshold: 0.1,
+    });
+    const [paragraphRef6, paragraphVisible6] = useIntersectionObserver({
+        threshold: 0.1,
+    });
+    const [teamRef, teamVisible] = useIntersectionObserver({ threshold: 0.1 });
+    return (
+        <>
+            <CenteredWrap>
+                <Title ref={titleRef} isVisible={titleVisible}>
+                    About Us
+                </Title>
+                <StyledH2 ref={missionRef} isVisible={missionVisible}>
+                    Our mission...
+                </StyledH2>
+                <StyledParagraph
+                    ref={paragraphRef1}
+                    isVisible={paragraphVisible1}
+                >
+                    We want to make the work of tattoo artists easier.
+                </StyledParagraph>
+                <StyledParagraph
+                    ref={paragraphRef2}
+                    isVisible={paragraphVisible2}
+                >
+                    Lots of tattooers rely on social media for finding work.
+                    Nowadays social media is heavily controlled by algorithms.
+                    Rules, that are often hard to understand, furthermore they
+                    often don't work into your favour, even if you play by their
+                    rules.
+                </StyledParagraph>
+
+                <StyledParagraph
+                    ref={paragraphRef3}
+                    isVisible={paragraphVisible3}
+                >
+                    As a result, they can end up with a lot less work. Sometimes
+                    even get "shadow banned" by the algorithm - often for
+                    reasons they don't know - which means their content is shown
+                    to little to no people.
+                </StyledParagraph>
+
+                <StyledParagraph
+                    ref={paragraphRef4}
+                    isVisible={paragraphVisible4}
+                >
+                    We believe this can be changed.
+                </StyledParagraph>
+
+                <StyledParagraph
+                    ref={paragraphRef5}
+                    isVisible={paragraphVisible5}
+                >
+                    We believe tattoo artists should have the opportunity to
+                    solely focus on creating. On letting their imagination flow
+                    and putting their work out without having to play by the
+                    opaque rules of algorithms.
+                </StyledParagraph>
+
+                <StyledH2 ref={teamRef} isVisible={teamVisible}>
+                    The team
+                </StyledH2>
+                <StyledParagraph
+                    ref={paragraphRef6}
+                    isVisible={paragraphVisible6}
+                >
+                    We are a team of developers standing at the dawn of our
+                    career. This website is one of our early attempts to
+                    materialize what we have learned so far.
+                </StyledParagraph>
+            </CenteredWrap>
+        </>
+    );
+};
+
+export default About;
---
+++ frontend/src/components/Home/styles/about.js
@@ -0,0 +1,88 @@
+import styled, { css } from "styled-components";
+
+// media queries
+const size = {
+    mobileS: "320px",
+    mobileM: "375px",
+    mobileL: "425px",
+    tablet: "768px",
+    laptop: "1024px",
+    laptopL: "1440px",
+    desktop: "2560px",
+};
+
+const device = {
+    mobileS: `(min-width: ${size.mobileS})`,
+    mobileM: `(min-width: ${size.mobileM})`,
+    mobileL: `(min-width: ${size.mobileL})`,
+    tablet: `(min-width: ${size.tablet})`,
+    laptop: `(min-width: ${size.laptop})`,
+    laptopL: `(min-width: ${size.laptopL})`,
+    desktop: `(min-width: ${size.desktop})`,
+    desktopL: `(min-width: ${size.desktop})`,
+};
+
+// Shared animation styles
+const fadeIn = css`
+    opacity: 1;
+    transform: translateY(0);
+    transition: opacity 0.6s ease-in, transform 0.6s ease-in;
+`;
+
+const fadeOut = css`
+    opacity: 0;
+    transform: translateY(20px);
+`;
+
+export const CenteredWrap = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: justify;
+    padding: 5rem 2rem;
+    /* background-color: rgb(229, 209, 188); */
+    background-color: var(--background2-color);
+    @media ${device.tablet} {
+        /* background-color: lightblue; */
+        padding: 5 15rem;
+    }
+
+    @media ${device.laptop} {
+        /* background-color: lightcoral; */
+        padding: 5rem 20rem;
+    }
+
+    @media ${device.laptopL} {
+        /* background-color: lightgreen; */
+        padding: 5rem 55rem;
+    }
+`;
+
+export const Title = styled.h1`
+    font-family: var(--font-title);
+    /* font-size: 4.5rem; */
+    font-size: clamp(4.5rem, 5vw, 6.6rem);
+    border-bottom: 1px solid black;
+    padding: 0 7rem 2rem 7rem;
+    margin-bottom: 5rem;
+    ${({ isVisible }) => (isVisible ? fadeIn : fadeOut)}
+`;
+
+export const StyledH2 = styled.i`
+    font-family: "Lora", serif;
+    color: grey;
+    font-size: 2.7rem;
+    font-weight: 400;
+    margin: 5rem 0;
+    ${({ isVisible }) => (isVisible ? fadeIn : fadeOut)}
+`;
+
+export const StyledParagraph = styled.p`
+    font-family: "Lato", sans-serif;
+    font-size: 2rem;
+    padding: 0 2.5rem 2rem 2.5rem;
+    @media ${device.tablet} {
+        margin-bottom: 5rem;
+    }
+    ${({ isVisible }) => (isVisible ? fadeIn : fadeOut)}
+`;
